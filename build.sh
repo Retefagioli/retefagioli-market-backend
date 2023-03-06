8f126d2b24bdb2d917dc9599dd929dd7e8cf0bf4
Blessium <alessandro.melgar04@gmail.com>
2023-03-06 18:16:12 +0100

    Updated the build script

--- build.sh
+++ build.sh
@@ -5,9 +5,20 @@ set -e
 PROJECT_NAME="retefagioli-market-backend"
 VERSION="0.0.1-SNAPSHOT"
 
+if [ -z "$(pgrep docker)" ]
+then
+  echo "[!] ERROR: Start docker before running this script."
+  exit 1
+fi
+
 echo "[+] Building the application JAR..."
 ./mvnw clean package -DskipTests
 echo "[+] Copying JAR to src/main/docker"
 cp ./target/$PROJECT_NAME-$VERSION.jar ./src/main/docker
 cd ./src/main/docker
 
+if [ -z "$(docker images | grep 17-jre-alpine)"]
+then
+  echo "[+] Installing docker image eclipse-temurin:17-jre-alpine"
+  docker pull eclipse-temurin:17-jre-alpine
+fi
