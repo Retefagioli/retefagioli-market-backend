4d95fada6c99f1bb8aafb7c439db2dbdd8666f94
Blessium <alessandro.melgar04@gmail.com>
2023-03-09 20:10:47 +0100

    Improved user service by separating interface from implementation

--- src/main/java/io/exsuslabs/retefagiolimarketbackend/service/UserService.java
+++ src/main/java/io/exsuslabs/retefagiolimarketbackend/service/UserService.java
@@ -1,35 +1,13 @@
 package io.exsuslabs.retefagiolimarketbackend.service;
 
-import io.exsuslabs.retefagiolimarketbackend.model.UserModel;
-import io.exsuslabs.retefagiolimarketbackend.repository.UserRepository;
 import io.exsuslabs.retefagiolimarketbackend.request.UserFullInfoRequest;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.dao.DataAccessException;
-import org.springframework.stereotype.Service;
 
 import java.util.Optional;
+import java.util.UUID;
 
-@Service
-public class UserService {
-
-    @Autowired
-    UserRepository _userRepository;
-
-    public Optional<String> createUser(UserFullInfoRequest userFullInfoRequest) {
-        UserModel user = new UserModel();
-        System.out.println(userFullInfoRequest.getId());
-        user.setId(userFullInfoRequest.getId());
-        user.setName(userFullInfoRequest.getName());
-        user.setSurname(userFullInfoRequest.getSurname());
-        user.setEmail(userFullInfoRequest.getEmail());
-        user.setDob(userFullInfoRequest.getDob());
-        user.setPhone(userFullInfoRequest.getPhone());
-        user.setAddress(userFullInfoRequest.getAddress());
-        try {
-            _userRepository.save(user);
-        } catch (DataAccessException e) {
-            return Optional.of(e.getMessage());
-        }
-        return Optional.empty();
-    }
+public interface UserService {
+    Optional<String> createUser(UserFullInfoRequest userFullInfoRequest);
+    Optional<String> updateUser(UserFullInfoRequest userFullInfoRequest);
+    Optional<String> getUser(UUID id);
+    Optional<String> deleteUser(UUID id);
 }
