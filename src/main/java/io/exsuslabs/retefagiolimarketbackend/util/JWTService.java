f1577e17298c6abb44e1ba9e0754391b24845c87
Blessium <alessandro.melgar04@gmail.com>
2023-03-16 21:21:17 +0100

    JWT service for creating and verification of tokens

---
+++ src/main/java/io/exsuslabs/retefagiolimarketbackend/util/JWTService.java
@@ -0,0 +1,49 @@
+package io.exsuslabs.retefagiolimarketbackend.util;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTVerifier;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.auth0.jwt.interfaces.DecodedJWT;
+import io.exsuslabs.retefagiolimarketbackend.model.UserModel;
+import org.hibernate.query.sqm.TemporalUnit;
+
+import java.time.Instant;
+import java.time.temporal.TemporalAmount;
+import java.util.Optional;
+
+public class JWTService {
+    private static final String SECRET_KEY = "";
+    private static final String ISSUER = "EXSUS-LABS-001";
+    private static final Algorithm ALGO = Algorithm.HMAC256(SECRET_KEY);
+    public static Optional<String> createJWT(UserModel userModel) {
+        try {
+            return Optional.of(JWT
+                    .create()
+                    .withSubject(userModel.getName())
+                    .withIssuer(ISSUER)
+                    .withExpiresAt(Instant.now().plusSeconds(300))
+                    .withIssuedAt(Instant.now())
+                    .withClaim("id", userModel.getId().toString())
+                    .withClaim("email", userModel.getEmail())
+                    .sign(ALGO)
+            );
+        } catch (JWTCreationException e) {
+            return Optional.empty();
+        }
+    }
+
+    public static Optional<DecodedJWT> verifyJWT(String token) {
+        try {
+            JWTVerifier verifier = JWT.require(ALGO)
+                    .withIssuer(ISSUER)
+                    .withClaimPresence("id")
+                    .withClaimPresence("email")
+                    .build();
+            return Optional.of(verifier.verify(token));
+        } catch (JWTVerificationException e) {
+            return Optional.empty();
+        }
+    }
+}
