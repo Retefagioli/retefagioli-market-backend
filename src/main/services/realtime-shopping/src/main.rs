0359f0c61a34df359cbc1bbab36b2253e4ecb09f
Blessium <alessandro.melgar04@gmail.com>
2023-03-19 15:52:21 +0100

    Updated src/main.rs

--- src/main/services/realtime-shopping/src/main.rs
+++ src/main/services/realtime-shopping/src/main.rs
@@ -1,3 +1,11 @@
-fn main() {
-    println!("Hello, world!");
+pub mod products;
+
+use actix_web::{App, HttpServer};
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| App::new().service(products::get_products_apis()))
+        .bind(("127.0.0.1", 8080))?
+        .run()
+        .await
 }
