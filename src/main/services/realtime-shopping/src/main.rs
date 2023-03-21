07a5ae4980df01153da528f622478e9ce57f0232
Blessium <alessandro.melgar04@gmail.com>
2023-03-21 13:58:15 +0100

    Updated docker-compose.yaml

--- src/main/services/realtime-shopping/src/main.rs
+++ src/main/services/realtime-shopping/src/main.rs
@@ -7,7 +7,7 @@ use actix_web::{App, HttpServer};
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| App::new().service(products::get_products_apis()))
-        .bind(("127.0.0.1", 8080))?
+        .bind(("0.0.0.0", 8080))?
         .run()
         .await
 }
