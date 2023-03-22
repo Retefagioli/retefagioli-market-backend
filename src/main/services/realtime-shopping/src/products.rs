3efd5b14f2a14400d5fc231f28ce8e5035e90c6d
Blessium <alessandro.melgar04@gmail.com>
2023-03-22 18:10:03 +0100

    Updated products api with the new ProductService

--- src/main/services/realtime-shopping/src/products.rs
+++ src/main/services/realtime-shopping/src/products.rs
@@ -1,5 +1,6 @@
 mod api;
 mod models;
+mod service;
 
 use actix_web::{web, Scope};
 
--- src/main/services/realtime-shopping/src/products/api.rs
+++ src/main/services/realtime-shopping/src/products/api.rs
@@ -1,6 +1,9 @@
 use crate::database::{constant, Database};
 use crate::products::models::Product;
+use crate::products::service::ProductService;
+use crate::service::CrudService;
 use actix_web::{delete, get, post, put, web, Responder};
+
 
 #[get("/{id}")]
 pub async fn get_product(id: web::Path<String>) -> impl Responder {
@@ -10,12 +13,7 @@ pub async fn get_product(id: web::Path<String>) -> impl Responder {
 
 #[post("")]
 pub async fn add_product(product: web::Json<Product>) -> impl Responder {
-    let database = Database::get_client()
-        .await
-        .unwrap()
-        .database(constant::DATABASE_NAME);
-    let collection = database.collection::<Product>("products");
-    collection.insert_one(product.clone(), None).await;
+    ProductService::save(product.clone()).await.unwrap(); 
     "CIAO"
 }
 
