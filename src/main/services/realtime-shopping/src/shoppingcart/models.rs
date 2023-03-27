0a307c37e2b0e221421c44a2bc1d440c4cf61b1f
Blessium <alessandro.melgar04@gmail.com>
2023-03-27 11:52:51 +0200

    Added first api

--- src/main/services/realtime-shopping/src/shoppingcart/models.rs
+++ src/main/services/realtime-shopping/src/shoppingcart/models.rs
@@ -1,17 +1,31 @@
-use std::collections::HashSet;
+use std::collections::HashMap;
+use serde::{Deserialize, Serialize};
 
 type ProductId = String;
 type Quantity = u64;
 
+#[derive(Serialize, Deserialize)]
 pub enum CartState {
     Open,
     InProgress,
     Closed,
 }
 
+#[derive(Serialize, Deserialize)]
 pub struct ShoppingCart {
     cart_id: String, 
     user_id: String,
-    product_list: HashSet<ProductId, Quantity>,
+    product_list: HashMap<ProductId, Quantity>,
     state: CartState,
+}
+
+impl ShoppingCart {
+    pub fn new(user_id: &str) -> Self {
+        ShoppingCart { 
+            cart_id: "JUIS".to_string(), 
+            user_id: user_id.to_string(), 
+            product_list: HashMap::new(), 
+            state: CartState::Open 
+        }
+    }
 }
