e2305570787b95ba9ce2221cfd89f251c79fcc9f
Blessium <alessandro.melgar04@gmail.com>
2023-03-31 16:01:33 +0200

    Added all api to scope

--- src/main/services/realtime-shopping/src/shoppingcart.rs
+++ src/main/services/realtime-shopping/src/shoppingcart.rs
@@ -1,2 +1,13 @@
 mod api;
 pub mod models;
+
+use actix_web::{web, Scope};
+
+pub fn get_shoppingcart_api() -> Scope {
+    web::scope("/cart")
+        .service(api::get_shoppingcart)
+        .service(api::init_shoppingcart)
+        .service(api::add_product_to_cart)
+        .service(api::delete_product_from_cart)
+        .service(api::update_product_in_cart)
+}
