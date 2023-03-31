e05d113948d9fbcf66d84ae51ba9949d5ae04090
Blessium <alessandro.melgar04@gmail.com>
2023-03-31 16:00:42 +0200

    Added shoppingcart service to web application

--- src/main/services/realtime-shopping/src/main.rs
+++ src/main/services/realtime-shopping/src/main.rs
@@ -8,8 +8,12 @@ use actix_web::{App, HttpServer};
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| App::new().service(products::get_products_apis()))
-        .bind(("0.0.0.0", 8080))?
-        .run()
-        .await
+    HttpServer::new(|| {
+        App::new()
+            .service(products::get_products_apis())
+            .service(shoppingcart::get_shoppingcart_api())
+    })
+    .bind(("0.0.0.0", 8080))?
+    .run()
+    .await
 }
