66d9772dc92927a24ac4f1f81564186a8e108b98
Blessium <alessandro.melgar04@gmail.com>
2023-04-05 19:23:29 +0200

    Added java test infrastructure

--- services/retefagioli-market-backend/src/test/java/io/exsuslabs/retefagiolimarketbackend/TestConfiguration.java
+++ services/retefagioli-market-backend/src/test/java/io/exsuslabs/retefagiolimarketbackend/TestConfiguration.java
@@ -1,2 +1,16 @@
-package io.exsuslabs.retefagiolimarketbackend;public class TestConfiguration {
+package io.exsuslabs.retefagiolimarketbackend;
+
+import org.mockito.Mockito;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+import javax.sql.DataSource;
+
+@org.springframework.boot.test.context.TestConfiguration
+@EnableWebMvc
+public class TestConfiguration {
+    @Bean
+    public DataSource dataSource () {
+        return Mockito.mock(DataSource.class);
+    }
 }
--- services/retefagioli-market-backend/src/test/java/io/exsuslabs/retefagiolimarketbackend/UserMangementTest.java
+++ services/retefagioli-market-backend/src/test/java/io/exsuslabs/retefagiolimarketbackend/UserMangementTest.java
@@ -1,2 +1,69 @@
-package io.exsuslabs.retefagiolimarketbackend;public class UserMangementTest {
+package io.exsuslabs.retefagiolimarketbackend;
+
+import com.google.gson.Gson;
+import io.exsuslabs.retefagiolimarketbackend.controller.UserController;
+import io.exsuslabs.retefagiolimarketbackend.model.UserModel;
+import io.exsuslabs.retefagiolimarketbackend.repository.UserRepository;
+import io.exsuslabs.retefagiolimarketbackend.request.UserFullInfoRequest;
+import io.exsuslabs.retefagiolimarketbackend.service.UserService;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+import java.nio.charset.Charset;
+import java.util.Optional;
+import java.util.UUID;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+
+@SpringBootTest(classes = {
+        TestConfiguration.class,
+        UserController.class
+})
+@AutoConfigureMockMvc
+@EnableWebMvc
+public class UserMangementTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @MockBean
+    UserService userService;
+
+    @Test
+    void contextLoads() {
+    }
+
+
+    @Test
+    public void exampleTest() throws Exception {
+
+        UserFullInfoRequest fullInfoRequest = new UserFullInfoRequest();
+        UUID testingUUID = UUID.randomUUID();
+        fullInfoRequest.setId(testingUUID);
+        fullInfoRequest.setName("test");
+        fullInfoRequest.setSurname("last_test");
+        fullInfoRequest.setEmail("test@test.com");
+
+        Mockito.when(userService.createUser(fullInfoRequest)).thenReturn(Optional.empty());
+
+        Gson gson = new Gson();
+
+        mockMvc.perform(post("/user")
+                        .content(gson.toJson(fullInfoRequest))
+                        .accept(MediaType.APPLICATION_JSON)
+                        .contentType("application/json")
+                        .characterEncoding(Charset.defaultCharset()))
+                .andExpect(MockMvcResultMatchers.status().isCreated())
+                .andReturn();
+    }
 }
